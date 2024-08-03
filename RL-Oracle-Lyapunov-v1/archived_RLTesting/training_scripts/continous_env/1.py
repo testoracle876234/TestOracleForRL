f36d5bd14b215905b2d4e2c12ffb2b7fe6ac4014
testoracle876234 <166719847+testoracle876234@users.noreply.github.com>
2024-08-03 14:49:01 +0800

    removed old versions

--- RL-Oracle-Lyapunov-v1/archived_RLTesting/training_scripts/continous_env/1.py
+++
@@ -1,9 +0,0 @@
-import gymnasium as gym
-
-env = gym.make('Pendulum-v1')
-env.reset()
-
-action = env.action_space.sample()
-result1, result2,result3,result4,result5 = env.step(action)
-
-print(result1, result2, result3, result4, result5)
